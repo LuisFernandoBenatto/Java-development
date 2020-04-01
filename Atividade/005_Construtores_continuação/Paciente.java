58551f00fee5606f245c74a41f213743d9cbf336
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-04-01 18:11:08 -0300

    Add files via upload

---
+++ Atividade/005_Construtores_continuação/Paciente.java
@@ -0,0 +1,11 @@
+package construtores_continuação;
+
+public class Paciente {
+    String nome;
+    int idade;
+    
+    Paciente(){  
+        idade = 0;  
+        nome = new String(""); 
+    } 
+}
