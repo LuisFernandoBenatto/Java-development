9758c084627beca94932fd681afaed698a4963c6
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-09-24 00:29:23 -0300

    ☕ Hello World em Java

--- aulasDeJava/000_Hello_World/HelloWorld.java
+++ aulasDeJava/000_Hello_World/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         Class_Hello hello = new Class_Hello();
         System.out.print(hello.Hello);
+
+        System.out.println("Hello, World!!!");
     }
     
 }
