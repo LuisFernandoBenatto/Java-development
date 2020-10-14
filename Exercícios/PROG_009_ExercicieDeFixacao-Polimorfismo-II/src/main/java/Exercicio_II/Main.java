95b153fafc4d1713ebc33799db558257028bf5b5
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-10-13 22:30:31 -0300

    ☕ Segunda lista de Poliformismo em JAVA

---
+++ Exercícios/PROG_009_ExercicieDeFixacao-Polimorfismo-II/src/main/java/Exercicio_II/Main.java
@@ -0,0 +1,25 @@
+/*
+    2 Ao chamar o método toString( ) em um objeto de uma classe qualquer o 
+    retorno é composto pelo nome do pacote + nome da classe + uma representação
+    hexadecimal.
+        Exemplo: veiculo.Moto@6d9c638.
+    Por que ao chamar o método toString( ) em um objeto do tipo String isto é
+    diferente?
+
+ */
+package Exercicio_II;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String string = new String();
+        Classe classe = new Classe();
+        
+        System.out.println("-----------------------------------------------");
+        System.out.println(string.toString()); // Redundant String.toString()
+        System.out.println("");
+        System.out.println(classe.toString());
+        System.out.println("-----------------------------------------------");
+    }
+    
+}
