80c9dc624b2e67956360c9e58a0169cdf823237f
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-10-14 15:24:21 -0300

    ☕ Estruturas de Listas Dinâmicas  ( ED )

---
+++ Exercícios/ES_012_EstruturaDinamicas/src/main/java/EstruturaDinamicas_Inserir/Lista.java
@@ -0,0 +1,32 @@
+package EstruturaDinamicas_Inserir;
+
+public class Lista {
+    private No inicio;
+    private No fim;
+    
+    public void inserir(No n) {
+        if(inicio == null || fim == null) {
+            inicio = n;
+            fim = n;
+            System.out.println("Elemento inserido com sucesso!!!");
+        } else {
+            fim.proximo = n;
+            fim = n;
+            System.out.println("Elemento inserido no final... "
+                    + "Inserido com sucesso!");
+        }
+    }
+    public void imprimirLista() {
+        No aux = inicio;
+        System.out.println("");
+        System.out.println("Inicio da Lista: "  + inicio.getClass() + "@" +
+                                      Integer.toHexString(inicio.hashCode()));
+        System.out.println("Fim da Lista: "  + fim.getClass() + "@" +
+                                         Integer.toHexString(fim.hashCode()));
+        System.out.println("");
+        while(aux != null) {
+            System.out.println(aux.toString());
+            aux = aux.proximo;
+        }
+    }
+}
---
+++ Exercícios/ES_012_EstruturaDinamicas/src/main/java/EstruturaDinamicas_Inserir/Main.java
@@ -0,0 +1,64 @@
+/**
+ * Estrutura de Dados Dinâmica ->
+ * Estrutura que aloca recursos de memória 
+ * conforme há necessidade de inclusão de novos 
+ * elementos
+ * 
+ * Ponteiros são fundamentais para manipular
+ * estas estruturas
+ * 
+ * Estrutura estática: 
+ * - Vetor com tamanho pré-definido
+ * - Poucos ponteiros somente para indicar
+ * o inicio ou fim da estrutura
+  1   2  3  4   5
+  • | • | • | • | •
+  inicio          fim
+ * 
+ * Ponteiros eram definidos como inteiros,
+ * pois representam um índice no vetor
+ * 
+ * Estrutura Dinâmicas:
+ * 
+ * NÓ -> |A| -> |B| -> |C| -> |D| ->  |F|fim
+ * 
+ * Em Estrutura dinâmica os ponteiros são
+ * representações dos endereços e memória dos 
+ * objetos que fazem parte da estrutura de dados
+ * 
+ */
+package EstruturaDinamicas_Inserir;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        
+        No n = new No("A", 1);
+        lista.inserir(n);
+        n = new No("B", 2);
+        lista.inserir(n);
+        
+        lista.imprimirLista();
+        System.out.println("-----------------------------------------------");
+        
+        n = new No("C", 3);
+        lista.inserir(n);
+        n = new No("D", 4);
+        lista.inserir(n);
+        
+        lista.imprimirLista();
+        System.out.println("-----------------------------------------------");
+    }
+    
+}
+/** LISTA ENCADEADA DINÂMICA
+ * 
+ *  Criar / alocar espaços em memória de forma
+ * dinâmica (em tempo de execução);
+ *  Os elementos da lista estarão conectados 
+ * (encadeados) por meio de ponteiros;
+ * 
+ * Ponteiros: Inicio e Fim
+ * De valor Null inicialmente
+ */
