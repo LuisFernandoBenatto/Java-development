4b3016d293b0deb337d8fb2d9443655f57160609
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-12-11 21:27:35 -0300

    ☕Exercicio IV da lista de ED

--- Exercícios/ListaDeExercicios_III_EstruturaDeDados/src/main/java/Exercicio_IV/EncontrarSenha.java
+++ Exercícios/ListaDeExercicios_III_EstruturaDeDados/src/main/java/Exercicio_IV/EncontrarSenha.java
@@ -1,5 +1,12 @@
 package Exercicio_IV;
 
 public class EncontrarSenha {
-    
+    public int encontrarSenha(int numero, int senha) {
+        if(numero == senha) {
+            return numero;
+        } else {
+            System.out.println("Numero: " + numero);
+            return encontrarSenha(numero + 1, senha);
+        }
+    }
 }
--- Exercícios/ListaDeExercicios_III_EstruturaDeDados/src/main/java/Exercicio_IV/Main.java
+++ Exercícios/ListaDeExercicios_III_EstruturaDeDados/src/main/java/Exercicio_IV/Main.java
@@ -2,6 +2,30 @@ package Exercicio_IV;
 
 public class Main {
     public static void main(String[] args) {
+        EncontrarSenha encontrar = new EncontrarSenha();
+        System.out.println("Teste com 1 dígitos");
+        int senha = 1;
+        System.out.println("Senha: " + 
+                encontrar.encontrarSenha(0, senha));
         
+        System.out.println("Teste com 2 dígitos");
+        senha = 12;
+        System.out.println("Senha: " + 
+                encontrar.encontrarSenha(0, senha));
+        
+        System.out.println("Teste com 3 dígitos");
+        senha = 123;
+        System.out.println("Senha: " + 
+                encontrar.encontrarSenha(0, senha));
+        
+        System.out.println("Teste com 4 dígitos");
+        senha = 1234;
+        System.out.println("Senha: " + 
+                encontrar.encontrarSenha(0, senha));
+        
+        System.out.println("Teste com 5 dígitos");
+        senha = 12345;
+        System.out.println("Senha: " + 
+                encontrar.encontrarSenha(0, senha));
    }   
 }
