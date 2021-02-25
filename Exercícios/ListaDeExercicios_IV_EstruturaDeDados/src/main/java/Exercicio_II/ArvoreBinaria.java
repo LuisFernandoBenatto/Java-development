8b313ecb7d9d8e99a1dbba880996793eb060d0c4
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2021-02-24 23:26:53 -0300

    ListaDeExercicios_IV_EstruturaDeDados

---
+++ Exercícios/ListaDeExercicios_IV_EstruturaDeDados/src/main/java/Exercicio_II/ArvoreBinaria.java
@@ -0,0 +1,62 @@
+package Exercicio_II;
+
+public class ArvoreBinaria<TIPO extends Comparable> {
+    private Elemento<TIPO> raiz;
+    
+    public ArvoreBinaria() {
+        this.raiz = null;
+    }
+    public Elemento<TIPO> getRaiz() {
+        return raiz;
+    }
+    public void setRaiz(Elemento<TIPO> raiz) {
+        this.raiz = raiz;
+    }  
+    public void adicionarElemento(TIPO valor) {
+        Elemento<TIPO> novoElemento = new Elemento<TIPO>(valor);
+        if(raiz == null) {
+            this.raiz = novoElemento;
+        } else {
+            Elemento<TIPO> atual = this.raiz;
+            while(true) {
+                if(novoElemento.getValor().compareTo(atual.getValor()) == -1) 
+                {
+                    if(atual.getEsquerda() != null) {
+                        atual = atual.getEsquerda();
+                    } else {
+                        atual.setEsquerda(novoElemento);
+                        break;
+                    }
+                } else {
+                    if(atual.getDireita() != null) {
+                        atual = atual.getDireita();
+                    } else {
+                        atual.setDireita(novoElemento);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    public void imprimirPreOrdem(Elemento<TIPO> atual) {
+        if(atual != null) {
+            System.out.print(atual.getValor() + " ");
+            imprimirPreOrdem(atual.getEsquerda());
+            imprimirPreOrdem(atual.getDireita());
+        } 
+    }
+    public void imprimirEmOrdem(Elemento<TIPO> atual) {
+        if(atual != null) {
+            imprimirEmOrdem(atual.getEsquerda());
+            System.out.print(atual.getValor() + " ");
+            imprimirEmOrdem(atual.getDireita());
+        }   
+    }
+    public void imprimirPosOrdem(Elemento<TIPO> atual) {
+        if(atual != null) {
+            imprimirPosOrdem(atual.getEsquerda());
+            imprimirPosOrdem(atual.getDireita());
+            System.out.print(atual.getValor() + " ");
+        }   
+    }
+}
---
+++ Exercícios/ListaDeExercicios_IV_EstruturaDeDados/src/main/java/Exercicio_II/Main.java
@@ -0,0 +1,37 @@
+package Exercicio_II;
+
+public class Main {
+    public static void main(String[] args) {
+        ArvoreBinaria<Integer> arvore = new ArvoreBinaria<Integer>();
+        arvore.adicionarElemento(50);
+        arvore.adicionarElemento(35);
+        arvore.adicionarElemento(40);
+        arvore.adicionarElemento(33);
+        
+        arvore.adicionarElemento(80);
+        arvore.adicionarElemento(3);
+        arvore.adicionarElemento(76);
+        arvore.adicionarElemento(90);
+        
+        arvore.adicionarElemento(56);
+        arvore.adicionarElemento(24);
+        arvore.adicionarElemento(13);
+        arvore.adicionarElemento(45);
+        
+        arvore.adicionarElemento(92);
+        arvore.adicionarElemento(32);
+        arvore.adicionarElemento(51);
+        arvore.adicionarElemento(81);
+        
+        arvore.adicionarElemento(88);
+      
+        System.out.println("Imprimir em Ordem: ");
+        arvore.imprimirEmOrdem(arvore.getRaiz());
+        System.out.println("");
+        System.out.println("Imprimir pos-Ordem: ");
+        arvore.imprimirPosOrdem(arvore.getRaiz());
+        System.out.println("");
+        System.out.println("Imprimir pre-Ordem: ");
+        arvore.imprimirPreOrdem(arvore.getRaiz());
+    }   
+}
